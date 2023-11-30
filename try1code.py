2b71384a33e5833d59fa48821498aa0fdc238917
Zilan Altindag <139772847+zilan1altindag@users.noreply.github.com>
2023-11-30 14:28:35 +0000

    Update try1code.py

--- try1code.py
+++ try1code.py
@@ -1,34 +1,32 @@
 import itertools
 import requests
 
-# Replace this URL with the actual URL for the script
-script_url = "http://10.0.0.6/ctf_deploy/aclive/ddjS7HkE9M/A.php"
+def try_combination(combination):
+    # Fill in the actual URL here
+    base_url = "http://your_server/your_path/flag.php"
+    
+    # Map parameters to their values in the combination
+    params = {param: str(value).lower() for param, value in zip(parameters, combination)}
+    
+    # Send a GET request to the server with the current combination
+    response = requests.get(base_url, params=params)
+    
+    # Print debug information
+    print(f"Combination: {combination}, Status Code: {response.status_code}, Result: {response.text}")
+    
+    return response.text
 
-# List of attributes and their possible values
-attributes = {
-    'OSA': ['True', 'False'],
-    'OSB': ['True', 'False'],
-    'OSC': ['True', 'False'],
-    'HSA': ['True', 'False'],
-    'HSB': ['True', 'False'],
-    'HSD': ['True', 'False'],
-}
+# List of parameters to be tested
+parameters = ["OSA",
+    "OSB",
+    "OSC",
+    "HSA",
+    "HSB",
+    "HSD"]
 
-# Generate all possible combinations of attribute values
-combinations = list(itertools.product(*attributes.values()))
+# Generate all possible combinations of True and False for the parameters
+combinations = list(itertools.product([True, False], repeat=len(parameters)))
 
-# Iterate through each combination and make a request to the script
-for combo in combinations:
-    # Create a dictionary with attribute names and values
-    params = dict(zip(attributes.keys(), combo))
-
-    # Make a request to the script with the current combination
-    response = requests.get(script_url, params=params)
-
-    # Check if the response contains the flag
-    if 'flag' in response.text:
-        print(f"Flag found with attributes: {params}")
-        print("Response:", response.text)
-        break
-    else:
-        print(f"Attempted attributes: {params}, Result: {response.text}")
+# Iterate through all combinations and test them
+for combination in combinations:
+    result = try_combination(combination)
